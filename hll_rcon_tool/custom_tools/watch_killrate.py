a551caa6544c847f7548b2e8629f9e79129047f5
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2024-11-14 02:29:19 +0100

    Update watch_killrate.py

--- hll_rcon_tool/custom_tools/watch_killrate.py
+++ hll_rcon_tool/custom_tools/watch_killrate.py
@@ -29,7 +29,31 @@ from custom_tools.common_functions import (
     team_view_stats
 )
 from custom_tools.common_translations import TRANSL
-from custom_tools.watch_killrate_config import *
+from custom_tools.watch_killrate_config import (
+    BOT_NAME,
+    KILLRATE_THRESHOLD,
+    LANG,
+    SERVER_CONFIG,
+    WATCH_INTERVAL_SECS,
+    WHITELIST_ARTILLERY,
+    WHITELIST_FLAGS
+)
+
+
+def get_match_elapsed_secs():
+    """
+    Return the number of seconds since MATCH START
+    """
+    secs_since_match_start = 1
+    logs_match_start = get_recent_logs(
+        action_filter=["MATCH START"],
+        exact_action=True
+    )
+    match_start_timestamp = logs_match_start["logs"][0]["timestamp_ms"] / 1000
+    secs_since_match_start = (
+        datetime.now() - datetime.fromtimestamp(match_start_timestamp)
+    ).total_seconds()
+    return secs_since_match_start
 
 
 def watch_killrate_loop():
@@ -64,52 +88,46 @@ def watch_killrate(
     Find the players whose kills per minute is over threshold
     Send a report to Discord if found
     """
-    # Get elapsed match time
-    mins_since_match_start = 1
-    logs_match_start = get_recent_logs(
-        action_filter=["MATCH START"],
-        exact_action=True
-    )
-    match_start_timestamp = logs_match_start["logs"][0]["timestamp_ms"] / 1000
-    mins_since_match_start = (
-        datetime.now() - datetime.fromtimestamp(match_start_timestamp)
-    ).total_seconds() / 60
 
     # Avoids weird data returned at the beginning of the game
-    if mins_since_match_start < 2:
+    if (get_match_elapsed_secs() / 60) < 2:
+        logger.info(
+            "Match started less than 2 minutes ago, waiting for %s mins",
+            round((WATCH_INTERVAL_SECS / 60), 2)
+        )
         return
 
     # Test the players
     for player in watched_players:
 
-        mins_since_connected = int(player["profile"]["current_playtime_seconds"]) / 60
-        mins_from_offdef = (int(player["offense"]) + int(player["defense"])) / 20
+        mins_on_map = (int(player["offense"]) + int(player["defense"])) / 20
 
-        # Don't test player : connected less than WATCH_INTERVAL_SECS ago
-        if mins_since_connected < (WATCH_INTERVAL_SECS / 60) :
+        # Don't test player : not been on map for at least a minute
+        if mins_on_map == 0:
             continue
 
         # Don't test player : no kills yet
         if int(player["kills"]) == 0:
             continue
 
-        # Don't test player : not been on map for at least a minute
-        if mins_from_offdef == 0:
+        # Don't test player : connected less than WATCH_INTERVAL_SECS ago
+        if int(player["profile"]["current_playtime_seconds"]) < WATCH_INTERVAL_SECS:
             continue
 
-        kills_per_minute = int(player["kills"]) / mins_from_offdef
+        kills_per_minute = int(player["kills"]) / mins_on_map
 
         if kills_per_minute > KILLRATE_THRESHOLD:
 
             # latest kills weapons (since last watch iteration)
-            time_now_minus_interval = (
-                datetime.now(timezone.utc) - timedelta(seconds=WATCH_INTERVAL_SECS)
-            )
             logs = get_recent_logs(
                 end=500,
                 player_search=player["name"],
                 action_filter=["KILL"],
-                min_timestamp=int(time_now_minus_interval.timestamp()),
+                min_timestamp=int(
+                    (
+                        datetime.now(timezone.utc) - timedelta(seconds=WATCH_INTERVAL_SECS)
+                    ).timestamp()
+                ),
                 exact_player_match=True,
                 exact_action=True
             )
@@ -118,15 +136,11 @@ def watch_killrate(
                 if log["player_name_1"] == player["name"] and not log["weapon"] in weapons:
                     weapons.append(log["weapon"])
 
-            # Test whitelisted flag presence on player's profile
+            # Test whitelisted flag
             whitelist_flag_present = False
-            try:
-                profile = get_player_profile(player["player_id"], 0)
-                for flag in WHITELIST_FLAGS:
-                    if player_has_flag(profile, flag):
-                        whitelist_flag_present = True
-            except :
-                logger.warning("Unable to check player profile for flags")
+            for flag in WHITELIST_FLAGS:
+                if player_has_flag((get_player_profile(player["player_id"], 0)), flag):
+                    whitelist_flag_present = True
 
             # Logs
             log_txt = (
@@ -136,53 +150,47 @@ def watch_killrate(
                 f"/{TRANSL[player['role']][LANG]}"
                 f" - Level : {player['level']}"
                 f" - {player['kills']} kills in"
-                f" {round(mins_from_offdef, 2)} minutes"
+                f" {round(mins_on_map, 2)} minutes"
                 f" ({round(kills_per_minute, 2)} kill/min)."
                 f" {TRANSL['lastusedweapons'][LANG]} : {', '.join(weapons)}",
             )
 
             # Log (player has a whitelist flag)
             if whitelist_flag_present:
-                logger.info(f"(whitelisted - flag) {log_txt}")
+                logger.info("(whitelisted - flag) %s", log_txt)
                 continue
 
             # Log (whitelisted artillery player)
             if WHITELIST_ARTILLERY:
                 if any(weapon in weapons for weapon in WEAPONS_ARTILLERY):
-                    logger.info(f"(whitelisted - artillery) {log_txt}")
+                    logger.info("(whitelisted - artillery) %s", log_txt)
                     continue
 
             # Log (non-whitelisted player)
-            logger.info(f"{log_txt}")
+            logger.info(log_txt)
 
-            # TODO : Flag the player
-
-            # Prepare Discord embed
+            # Create Discord embed
             if player["team"] == "axis":
                 team_symbol = "🟥"
             elif player["team"] == "allies":
                 team_symbol = "🟦"
-            embed_desc_txt = (
-                f"{team_symbol} {TRANSL[player['team']][LANG]} "
-                f"/ {player['unit_name']} "
-                f"/ {TRANSL[player['role']][LANG]}\n"
-                f"{player['kills']} kills "
-                f"/ {round(mins_from_offdef, 2)} min. "
-                f"(**{round(kills_per_minute, 2)} kill/min**)\n"
-                f"**{TRANSL['level'][LANG]} :** {player['level']}\n"
-                f"**{TRANSL['lastusedweapons'][LANG]} :**\n {', '.join(weapons)}"
-            )
-
-            embed_color = green_to_red(
-                kills_per_minute, min_value=KILLRATE_THRESHOLD, max_value=2
-            )
-
-            # Create Discord embed
             embed = discord.Embed(
                 title=player["name"],
                 url=get_external_profile_url(player["player_id"], player["name"]),
-                description=embed_desc_txt,
-                color=int(embed_color, base=16)
+                description=(
+                    f"{team_symbol} {TRANSL[player['team']][LANG]} "
+                    f"/ {player['unit_name']} "
+                    f"/ {TRANSL[player['role']][LANG]}\n"
+                    f"{player['kills']} kills "
+                    f"/ {round(mins_on_map, 2)} min. "
+                    f"(**{round(kills_per_minute, 2)} kill/min**)\n"
+                    f"**{TRANSL['level'][LANG]} :** {player['level']}\n"
+                    f"**{TRANSL['lastusedweapons'][LANG]} :**\n {', '.join(weapons)}"
+                ),
+                color=int(
+                    green_to_red(kills_per_minute, min_value=KILLRATE_THRESHOLD, max_value=2),
+                    base=16
+                )
             )
             embed.set_author(
                 name=BOT_NAME,
